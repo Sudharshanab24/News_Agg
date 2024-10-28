66912061da6cf6ac271a57f689c3212908bbdd84
sudharshana <sudhubalu04@gmail.com>
2024-10-28 15:55:57 +0530

    mongodb

--- server/server.js
+++ server/server.js
@@ -25,11 +25,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect('mongodb://localhost:27017/your_db_name', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("MongoDB connected successfully");
+})
+.catch(err => {
+  console.error("MongoDB connection error:", err);
+});
+
   const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
