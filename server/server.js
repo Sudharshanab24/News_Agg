af73119ad3381dce66824f2d5b1511e44d05c07b
sudharshana <sudhubalu04@gmail.com>
2024-10-28 16:10:20 +0530

    server

--- server/server.js
+++ server/server.js
@@ -25,16 +25,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("MongoDB connected successfully");
-})
-.catch(err => {
-  console.error("MongoDB connection error:", err);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+  });
 
   const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
