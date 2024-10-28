1d55141bab03740f0c7fdd6bc4bc778d4c1a5a1c
sudharshana <sudhubalu04@gmail.com>
2024-10-28 13:55:41 +0530

    changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 
 function Login({ onClose, onLogin }) {
   const [email, setEmail] = useState('');
