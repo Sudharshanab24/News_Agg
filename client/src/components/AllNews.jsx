a218043e6cfe6148f93e3a87b7b90ebb73b72d50
sudharshana <sudhubalu04@gmail.com>
2024-10-29 09:37:27 +0530

    allnews

--- client/src/components/AllNews.jsx
+++ client/src/components/AllNews.jsx
@@ -27,7 +27,7 @@ function AllNews() {
   useEffect(() => {
     setIsLoading(true);
     setError(null);
-    fetch(`${process.env.BACKEND_URL}all-news?page=${page}&pageSize=${pageSize}`)
+    fetch(`http://localhost:3000/all-news?page=${page}&pageSize=${pageSize}`)
       .then(response => {
         if (response.ok) {
           return response.json();
